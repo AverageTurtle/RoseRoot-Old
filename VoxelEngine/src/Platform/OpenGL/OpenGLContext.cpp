bc5fa693dfa053f4c9109ac2a6b3261777530492
Sam <samcashlamore@gmail.com>
2021-09-18 17:05:31 -0400

    Rendering A Triangle!

--- VoxelEngine/src/Platform/OpenGL/OpenGLContext.cpp
+++ VoxelEngine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -16,6 +16,11 @@ namespace VoxelEngine {
 		glfwMakeContextCurrent(m_windowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		VE_CORE_ASSERT(status, "Failed to initailize Glad!");
+
+		VE_CORE_INFO("OpenGL Info:");
+		VE_CORE_INFO("Vendor: {0}", glGetString(GL_VENDOR));
+		VE_CORE_INFO("Renderer: {0}", glGetString(GL_RENDERER));
+		VE_CORE_INFO("Version: {0}", glGetString(GL_VERSION));
 	}
 	void OpenGLContext::SwapBuffers()
 	{
--- VoxelEngine/src/VoxelEngine/EntryPoint.h
+++ VoxelEngine/src/VoxelEngine/EntryPoint.h
@@ -8,8 +8,6 @@ int main(int argc, char** argv)
 {
 	VoxelEngine::Log::Init();
 	VE_CORE_WARN("Initialized Log!");
-	int a = 5;
-	VE_CORE_INFO("Hello! Var={0}", a);
 
 	auto app = VoxelEngine::CreateApplication();
 	app->Run();
