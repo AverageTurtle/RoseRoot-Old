cc997927d11ffcd22dfd1f918ac1d86cc30fd391
Sam <samcashlamore@gmail.com>
2021-10-05 04:26:15 -0400

    FrameBuffer Attachment Clearing

--- RoseRoot/src/Platform/OpenGL/OpenGLFramebuffer.cpp
+++ RoseRoot/src/Platform/OpenGL/OpenGLFramebuffer.cpp
@@ -76,6 +76,17 @@ namespace RoseRoot {
 			return false;
 		}
 
+		static GLenum RoseFBTextureFormatToGL(FramebufferTextureFormat format)
+		{
+			switch (format)
+			{
+			case FramebufferTextureFormat::RGBA8: return GL_RGBA8;
+			case FramebufferTextureFormat::RED_INTEGER: return GL_RED_INTEGER;
+			}
+
+			RR_CORE_ASSERT(false);
+			return 0;
+		}
 	}
 
 	OpenGLFramebuffer::OpenGLFramebuffer(const FramebufferSpecification& spec)
@@ -198,6 +209,15 @@ namespace RoseRoot {
 		int pixelData;
 		glReadPixels(x, y, 1, 1, GL_RED_INTEGER, GL_INT, &pixelData);
 		return pixelData;
+
+	}
+
+	void OpenGLFramebuffer::ClearAttachment(uint32_t attachmentIndex, int value)
+	{
+		RR_CORE_ASSERT(attachmentIndex < m_ColorAttachments.size());
+
+		auto& spec = m_ColorAttachmentSpecifications[attachmentIndex];
+		glClearTexImage(m_ColorAttachments[attachmentIndex], 0, Utils::RoseFBTextureFormatToGL(spec.TextureFormat), GL_INT, &value);
 
 	}
 
