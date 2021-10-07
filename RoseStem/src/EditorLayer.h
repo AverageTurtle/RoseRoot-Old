9d9b14b050ee08c79700f253f457f8d5309f9d82
Sam <samcashlamore@gmail.com>
2021-10-07 06:59:34 -0400

    Play Button!

--- RoseStem/src/EditorLayer.h
+++ RoseStem/src/EditorLayer.h
@@ -26,7 +26,14 @@ namespace RoseRoot {
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
+
+		void OnScenePlay();
+		void OnSceneStop();
+
+		//UI Panels
+		void UI_Toolbar();
 	private:
+
 		Ref<Scene> m_ActiveScene;
 		Ref<Framebuffer> m_Framebuffer;
 
@@ -47,8 +54,17 @@ namespace RoseRoot {
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0, Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		//Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		//Editor Resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
