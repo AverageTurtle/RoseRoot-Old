2b21e55f52d212b744b05a046ee1012526d2c283
Sam <samcashlamore@gmail.com>
2022-03-23 20:59:38 -0400

    Sandbox has been emptied

--- Sandbox/src/Core/ChunkManager.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <RoseRoot.h>
-
-#include "Chunk.h"
-
--- Sandbox/src/Core/WorldConstants.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <RoseRoot.h>
-
-constexpr int CHUNK_SIZE = 32;
-constexpr int CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE;
-constexpr int CHUNK_VOLUME = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
